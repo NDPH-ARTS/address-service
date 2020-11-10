45b5112d106467b9d9b26dad441c525c6f25394a
Saj <saj.issa@gmail.com>
2020-11-10 10:47:24 +0000

    oauth2 implementation

--- src/main/java/uk/ac/ox/ctsu/arts/addressservice/config/SecurityConfig.java
+++ src/main/java/uk/ac/ox/ctsu/arts/addressservice/config/SecurityConfig.java
@@ -1,29 +1,54 @@
 package uk.ac.ox.ctsu.arts.addressservice.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 
-@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    public OAuth2UserService<OidcUserRequest, OidcUser> oidcUserService;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and()
+        http
                 .authorizeRequests()
+                .antMatchers("/version").permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .oauth2ResourceServer()
-                .jwt();
-    }
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/v2/api-docs",
-                "/configuration/ui",
-                "/swagger-resources/**",
-                "/configuration/security",
-                "/swagger-ui.html",
-                "/swagger-ui/**",
-                "/webjars/**");
+                .oauth2Login()
+                .userInfoEndpoint()
+                .oidcUserService(oidcUserService);
     }
 }
+
+//@Configuration
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.cors().and()
+//                .authorizeRequests()
+//                .anyRequest().authenticated()
+//                .and()
+//                .oauth2ResourceServer()
+//                .jwt();
+//    }
+//    @Override
+//    public void configure(WebSecurity web) throws Exception {
+//        web.ignoring().antMatchers("/v2/api-docs",
+//                "/configuration/ui",
+//                "/swagger-resources/**",
+//                "/configuration/security",
+//                "/swagger-ui.html",
+//                "/swagger-ui/**",
+//                "/webjars/**");
+//    }
+//}
