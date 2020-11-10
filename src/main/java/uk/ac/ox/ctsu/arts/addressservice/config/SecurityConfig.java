adbe436d7524abd482abd4404bc3eb4a9b25ddda
Saj <saj.issa@gmail.com>
2020-11-10 08:57:29 +0000

    oauth2 implementation

---
+++ src/main/java/uk/ac/ox/ctsu/arts/addressservice/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package uk.ac.ox.ctsu.arts.addressservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and()
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
+    }
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/v2/api-docs",
+                "/configuration/ui",
+                "/swagger-resources/**",
+                "/configuration/security",
+                "/swagger-ui.html",
+                "/swagger-ui/**",
+                "/webjars/**");
+    }
+}
